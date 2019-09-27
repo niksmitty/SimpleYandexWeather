66bed374535a326dd973c7189c4b61d983817c19
niksmitty <niksmitty@rambler.ru>
2019-09-27 16:20:13 +0500

    Added DayForecastCell (first version) for displaying information in week forecast module.

---
+++ SimpleYandexWeather/Views/DayForecast/DayForecastCell.swift
@@ -0,0 +1,40 @@
+//
+//  DayForecastCell.swift
+//  SimpleYandexWeather
+//
+//  Created by Никита on 26/09/2019.
+//  Copyright © 2019 Никита. All rights reserved.
+//
+
+import UIKit
+
+class DayForecastCell: UITableViewCell {
+    
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var dayTemperatureTitleLabel: UILabel!
+    @IBOutlet weak var dayIconImageView: UIImageView!
+    @IBOutlet weak var dayTemperatureValueLabel: UILabel!
+    @IBOutlet weak var nightTemperatureTitleLabel: UILabel!
+    @IBOutlet weak var nightIconImageView: UIImageView!
+    @IBOutlet weak var nightTemperatureValueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        dayTemperatureTitleLabel.text = "Температура днём"
+        nightTemperatureTitleLabel.text = "Температура ночью"
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func configureCell(with dayForecastItem: DayForecast) {
+        dayLabel.text = dayForecastItem.day
+        dayTemperatureValueLabel.text = "\(dayForecastItem.dayTemperature)ºC"
+        nightTemperatureValueLabel.text = "\(dayForecastItem.nightTemperature)ºC"
+    }
+    
+}
