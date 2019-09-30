c1ba6c4833422243d5e73c102704d39c49d2ddf0
niksmitty <niksmitty@rambler.ru>
2019-09-30 17:01:07 +0500

    Changed forecast storage service methods using new data about hours.

--- SimpleYandexWeather/Services/LocalPersistence/Services/Impl/ForecastStorageServiceImpl.swift
+++ SimpleYandexWeather/Services/LocalPersistence/Services/Impl/ForecastStorageServiceImpl.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2019 Никита. All rights reserved.
 //
 
+import RealmSwift
+
 final class ForecastStorageServiceImpl {
     
     private(set) var database: DatabaseManagerProtocol
@@ -29,7 +31,10 @@ extension ForecastStorageServiceImpl: ForecastStorageService {
                                 dayIconUrl: $0.dayIconUrl,
                                 dayTemperature: $0.dayTemperature,
                                 nightIconUrl: $0.nightIconUrl,
-                                nightTemperature: $0.nightTemperature)
+                                nightTemperature: $0.nightTemperature,
+                                hours: $0.hours.map {
+                                    HourForecastItem(hour: $0.hour, iconUrl: $0.iconUrl, temperature: $0.temperature)
+                                })
             }
     }
     
@@ -41,6 +46,19 @@ extension ForecastStorageServiceImpl: ForecastStorageService {
             dbItem.dayTemperature = item.dayTemperature
             dbItem.nightIconUrl = item.nightIconUrl
             dbItem.nightTemperature = item.nightTemperature
+            
+            let hourItems = item.hours.map { hourItem -> DBHourForecastItem in
+                let dbHourItem = DBHourForecastItem()
+                dbHourItem.hour = hourItem.hour
+                dbHourItem.iconUrl = hourItem.iconUrl
+                dbHourItem.temperature = hourItem.temperature
+                return dbHourItem
+            }
+            
+            let hours = List<DBHourForecastItem>()
+            hours.append(objectsIn: hourItems)
+            
+            dbItem.hours.append(objectsIn: hours)
             return dbItem
         }
         
